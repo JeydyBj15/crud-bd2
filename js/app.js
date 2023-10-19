9eed6f377e6bf55eb0cce51cd6930528741c4403
ARANGUSFT <Santiago.af505@gmail.com>
2023-10-19 08:34:38 -0500

    actu1

--- js/app.js
+++ js/app.js
@@ -42,6 +42,7 @@ const cargarTabla = () => {
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
+        <td> <img src="${item.imagen}" alt="${item.gift}" width="40" height="60"></td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
@@ -50,8 +51,8 @@ const cargarTabla = () => {
         </td>
         `;
 
-    fila.innerHTML = celdas;
-    cuerpoTabla.append(fila);
+      fila.innerHTML = celdas;
+      cuerpoTabla.append(fila);
   });
 };
 
